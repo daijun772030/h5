ae66e043519454d9700a28f4f870f9f40431aad8
daijun <904315004@qq.com>
2018-10-26 14:14:19 +0800

    代码第一次提交

---
+++ src/router/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home'
+import User from '@/pages/user'
+import All from '@/pages/all'
+import Law from '@/pages/law'
+import Privacy from '@/pages/privacy'
+import Agreement from '@/pages/agreement'
+import Rule from '@/pages/rule'
+import BusinessCode from '@/pages/businessCode'
+Vue.use(Router)
+
+export default new Router({
+    mode: 'history',
+    routes: [{
+            path: '/',
+            name: 'User',
+            component: User
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/all',
+            name: 'All',
+            component: All
+        },
+        {
+            path: '/law',
+            name: 'law',
+            component: Law
+        },
+        {
+            path: '/privacy',
+            name: 'privacy',
+            component: Privacy
+        },
+        {
+            path: '/agreement',
+            name: 'agreement',
+            component: Agreement
+        },
+        {
+            path: '/rule',
+            name: 'rule',
+            component: Rule
+        },
+        {
+            path: '/businessCode',
+            name: 'businessCode',
+            component: BusinessCode
+        }
+    ]
+})
